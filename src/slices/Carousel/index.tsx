06828539b2aef57573b32a80676f069c44ce922a
victor Macharia <Techvictor653@gmail.com>
2025-02-08 13:14:50 +0300

    feat: initialized the carousel slice

---
+++ src/slices/Carousel/index.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import { Content } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Carousel`.
+ */
+export type CarouselProps = SliceComponentProps<Content.CarouselSlice>;
+
+/**
+ * Component for "Carousel" Slices.
+ */
+const Carousel: FC<CarouselProps> = ({ slice }) => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      Placeholder component for carousel (variation: {slice.variation}) Slices
+    </section>
+  );
+};
+
+export default Carousel;
