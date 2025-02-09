cc1198865d5e3cf74bdac73951354454ffcb1fa2
victor Macharia <Techvictor653@gmail.com>
2025-02-09 13:02:09 +0300

    feat: Finished adding the content

--- src/slices/AlternatingText/index.tsx
+++ src/slices/AlternatingText/index.tsx
@@ -1,6 +1,15 @@
+"use client";
+
 import { FC } from "react";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import {
+  PrismicRichText,
+  PrismicText,
+  SliceComponentProps,
+} from "@prismicio/react";
+import { asText } from "@prismicio/client";
+
+import { Bounded } from "@/components/bounded";
 
 /**
  * Props for `AlternatingText`.
@@ -13,13 +22,35 @@ export type AlternatingTextProps =
  */
 const AlternatingText: FC<AlternatingTextProps> = ({ slice }) => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="alternating-text-container relative text-sky-950"
     >
-      Placeholder component for alternating_text (variation: {slice.variation})
-      Slices
-    </section>
+      <div>
+        <div className="relative grid">
+          {/* View goes here */}
+          {slice.primary.text_group.map((item, index) => (
+            <div
+              key={asText(item.heading)}
+              className="alternating-section grid h-screen place-items-center gap-x-12 md:grid-cols-2"
+            >
+              <div
+                className={index % 2 === 0 ? "col-start-1" : "md:col-start-2"}
+              >
+                <h2 className="text-6xl font-bold text-balance">
+                  <PrismicText field={item.heading} />
+                </h2>
+
+                <div className="mt-4 text-xl">
+                  <PrismicRichText field={item.body} />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Bounded>
   );
 };
 
