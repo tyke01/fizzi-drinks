0e4af3c05468bc1fa65af54ff1cce95fedee122b
victor Macharia <Techvictor653@gmail.com>
2025-02-09 18:20:03 +0300

    fix: Fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,19 @@
+import localFont from "next/font/local";
 
-import { repositoryName } from "@/prismicio";
 import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
 
-import Footer from "@/components/footer";
+import "./globals.css";
 import Header from "@/components/header";
 import ViewCanvas from "@/components/view-canvas";
-import "./globals.css";
+import Footer from "@/components/footer";
 
-
+const alpino = localFont({
+  src: "../../public/fonts/Alpino-Variable.woff2",
+  weight: "100 900",
+  display: "swap",
+  variable: "--font-alpino",
+});
 
 export default function RootLayout({
   children,
@@ -15,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="font-alpino">
+    <html lang="en" className={alpino.variable}>
       <body className="overflow-x-hidden bg-yellow-300">
         <Header />
         <main>
