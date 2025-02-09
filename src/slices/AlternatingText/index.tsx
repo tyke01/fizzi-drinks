67f3d9c6a1738d2a5939c1f87004c528a323e296
victor Macharia <Techvictor653@gmail.com>
2025-02-09 16:21:20 +0300

    update: finished working on alternating text slice

--- src/slices/AlternatingText/index.tsx
+++ src/slices/AlternatingText/index.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import { FC } from "react";
-import { Content } from "@prismicio/client";
+import { asText, Content } from "@prismicio/client";
 import {
   PrismicRichText,
   PrismicText,
   SliceComponentProps,
 } from "@prismicio/react";
-import { asText } from "@prismicio/client";
+import { FC } from "react";
 
 import { Bounded } from "@/components/bounded";
+import { View } from "@react-three/drei";
+import Scene from "./scene";
+import clsx from "clsx";
 
 /**
  * Props for `AlternatingText`.
@@ -25,10 +27,14 @@ const AlternatingText: FC<AlternatingTextProps> = ({ slice }) => {
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="alternating-text-container relative text-sky-950"
+      className="alternating-text-container relative bg-yellow-300 text-sky-950"
     >
       <div>
-        <div className="relative grid">
+        <div className="relative z-[100] grid">
+          <View className="alternating-text-view absolute top-0 left-0 h-screen w-full">
+            <Scene />
+          </View>
+
           {/* View goes here */}
           {slice.primary.text_group.map((item, index) => (
             <div
@@ -36,7 +42,10 @@ const AlternatingText: FC<AlternatingTextProps> = ({ slice }) => {
               className="alternating-section grid h-screen place-items-center gap-x-12 md:grid-cols-2"
             >
               <div
-                className={index % 2 === 0 ? "col-start-1" : "md:col-start-2"}
+                className={clsx(
+                  index % 2 === 0 ? "col-start-1" : "md:col-start-2",
+                  "rounded-lg p-4 backdrop-blur-lg max-md:bg-white/20",
+                )}
               >
                 <h2 className="text-6xl font-bold text-balance">
                   <PrismicText field={item.heading} />
